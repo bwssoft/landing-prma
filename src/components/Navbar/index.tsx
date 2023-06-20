8a2d0ff52f1cf6eaf74f085751c5b530468247e1
Diego Pereira <diego.pereira2806@hotmail.com>
2023-06-20 01:04:56 -0300

    feat: add style in Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
- import './styles.css';
+import "./styles.css";
+
+import logo from "../../assets/prma_white_label.png";
+
+import { HamburgerButton } from "@icon-park/react";
 
 const Navbar: React.FC = () => {
+  const [activeNav, setActiveNav] = useState(0);
+  function handleClick() {
+    if (activeNav == 0) {
+      setActiveNav(1);
+    }
+    if (activeNav == 1) {
+      setActiveNav(0);
+    }
+
+    console.log(activeNav);
+  }
+
+  function renderMobile() {
+    if (activeNav == 0) {
+      return (
+        <nav className="mobile">
+          <a className="active">Inicio</a>
+          <a className="active">Serviços</a>
+          <a className="active">Quem Somos</a>
+          <a className="active">Contato</a>
+        </nav>
+      );
+    }
+  }
   return (
-    <nav>
-      <div>Batata</div>
-      <ul>
-        <li>Inicio</li>
-        <li>Serviços</li>
-        <li>Quem Somos</li>
-        <li>Contato</li>
-      </ul>
-    </nav>
+    <>
+      <header>
+        <div>
+          <img src={logo} />
+        </div>
+        <nav>
+          <a className="active">Inicio</a>
+          <a className="active">Serviços</a>
+          <a className="active">Quem Somos</a>
+          <a className="active">Contato</a>
+        </nav>
+        <button onClick={() => handleClick()}>
+          <HamburgerButton size={"42px"} />
+        </button>
+      </header>
+      <div>{renderMobile()}</div>
+    </>
   );
 };
 
