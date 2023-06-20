c34da4b56dc9a35385a9ea934e9ce199ade1a388
Diego Pereira <diego.pereira2806@hotmail.com>
2023-06-20 12:41:20 -0300

    feat(Footer Component): add style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,57 @@
 import React from "react";
 
-// import { Container } from './styles';
-
+import "./styles.css";
+import logo from "../../assets/prma_white_label.png";
+import whatappIcon from "../../assets/whatsapp.png";
+import { PhoneTwo, LocalTwo } from "@icon-park/react";
 const Footer: React.FC = () => {
   return (
     <footer>
-        teste
-        
+      <div className="section1">
+        <img src={logo} />
+        <div>
+          <div>Soluções rápidas e efetivas na área de segurança.</div>
+          <div>
+            <h6>PRMA Federal</h6>
+            <h6>© Todos os direitos reservados</h6>
+          </div>
+        </div>
+      </div>
+      <div className="section2">
+        <h4>Serviços</h4>
+        <ul>
+          <li>
+            <a href="#">Central de monitoramento</a>
+          </li>
+          <li>
+            <a href="#">Pronta resposta veicular</a>
+          </li>
+          <li>
+            <a href="#">Pronta resposta patrimonial</a>
+          </li>
+          <li>
+            <a href="#">Assistência veicular 24 horas</a>
+          </li>
+          <li>
+            <a href="#">Acompanhamento jurídico</a>
+          </li>
+          <li>
+            <a href="#">Sindicância</a>
+          </li>
+        </ul>
+      </div>
+      <div className="section3">
+        <h4>Contato</h4>
+        <span>
+          <PhoneTwo />
+          <h4>+55 (11) 4195-0859</h4>
+        </span>
+        <h5>contato@prmafederal.com.br</h5>
+        <span>
+          <LocalTwo />
+          <h5>Alphaville Comercial Barueri - SP</h5>
+        </span>
+      </div>
     </footer>
   );
 };
